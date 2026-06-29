48121c969d4735ee07b28ae17a78f26375fde80b
kasidit_tim <joker642544@gmail.com>
2026-06-29 23:07:58 +0700

    feat: add gap constants

--- lib/core/theme/app_spacing.dart
+++ lib/core/theme/app_spacing.dart
@@ -1,11 +1,14 @@
-class AppSpacing {
-  static const xs = 4.0;
+import 'package:flutter/material.dart';
 
-  static const sm = 8.0;
+class Gap {
+  static const h4 = SizedBox(height: 4);
+  static const h8 = SizedBox(height: 8);
+  static const h12 = SizedBox(height: 12);
+  static const h16 = SizedBox(height: 16);
+  static const h24 = SizedBox(height: 24);
+  static const h32 = SizedBox(height: 32);
 
-  static const md = 16.0;
-
-  static const lg = 24.0;
-
-  static const xl = 32.0;
+  static const w4 = SizedBox(width: 4);
+  static const w8 = SizedBox(width: 8);
+  static const w16 = SizedBox(width: 16);
 }
